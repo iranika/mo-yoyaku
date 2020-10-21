23fc9484c827e9cf424656c8d346b3825ead10e6
iranika <7370486+iranika@users.noreply.github.com>
2020-10-21 19:18:46 +0900

    refactor goyoyaku.ps1

--- goyoyaku.ps1
+++ goyoyaku.ps1
@@ -9,17 +9,17 @@ $res = (Invoke-WebRequest $url -UseBasicParsing).Links | Where-Object { $_.href 
 if ([string]::IsNullOrEmpty($res)){
     Write-Error "Not found Element of a-tag, href match https://drive.google.com"
 }
-$target = @{}
-$target.href = $res.href
-$target.text = ([xml]$res.outerHTML).FirstChild.InnerText
-$target.timestamp = Get-Date -Format "yyyyMMdd"
 
 $json = Get-Content $jsonFile -Raw | ConvertFrom-Json
 
-if ($target.href -in $json.href){
+if ($res.href -in $json.href){
     Write-Information "Don't need update."
 }else{
+    $target = @{}
+    $target.href = $res.href
+    $target.text = ([xml]$res.outerHTML).FirstChild.InnerText
+    $target.timestamp = Get-Date -Format "yyyyMMdd"    
     $json += $target
 }
 
-$json | ConvertTo-Json | Out-File $jsonFile
+$json | ConvertTo-Json | Out-File $jsonFile -Encoding utf8
