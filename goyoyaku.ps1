bffe5c59d4f0314e4d386845b07de80fe3eea497
iranika109 <7370486+iranika@users.noreply.github.com>
2021-03-30 19:02:06 +0900

    Update goyoyaku.ps1

--- goyoyaku.ps1
+++ goyoyaku.ps1
@@ -24,5 +24,5 @@ if ($res.href -in $json.href){
 }
 
 $json | ConvertTo-Json | Out-File $jsonFile -Encoding utf8
-echo "goyoyaku = " | Out-File $jsFile -Encoding utf8
+echo "var goyoyaku = " | Out-File $jsFile -Encoding utf8
 $json | ConvertTo-Json | Out-File $jsFile -Encoding utf8 -Append
